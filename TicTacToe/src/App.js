6baaa43239b17ed9f6d45b8ee04aa06da099efdf
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-04-28 10:49:10 -0400

    Add Game component as main function which holds board and other game info

--- TicTacToe/src/App.js
+++ TicTacToe/src/App.js
@@ -11,7 +11,7 @@ function Square({value, onSquareClick}) {
   )
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -60,6 +60,19 @@ export default function Board() {
       </div>
     </>
   )
+}
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
 
 function calculateWinner(squares) {
